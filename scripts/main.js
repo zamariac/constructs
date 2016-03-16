115cdd622f5f9175d9fa041df3d738a4ac4e13f4
Maria Zamora <Zamora.Mariac@gmail.com>
2016-03-16 15:59:04 -0500

    initial

---
+++ scripts/main.js
@@ -0,0 +1,30 @@
+//1
+
+function playingCard (suit, value){
+	this.suit = suit || [ 'spades', 'hearts', 'clover', 'diamonds'];
+	this.value = value || [ 'ace', 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10, 'jack', 'queen','king'];
+
+	//input: suit and a value
+	//output: number and a string
+
+}
+
+//2
+
+function diceRoll(value){
+	this.value = [ 1 , 2 , 3 , 4 , 5 , 6];
+}
+
+var dice = new diceRoll();
+	//input: roll the die
+	//keep track of the current number 1-6
+
+	//output :
+
+//3
+
+function getProbabilities(diceOne, diceTwo){
+	
+
+}
+
